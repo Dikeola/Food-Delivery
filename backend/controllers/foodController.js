ba4189c7ad2240054c9cccf756d885d30b82be7e
Dikeola <Dikeola62@gmail.com>
2025-10-30 14:13:21 +0100

    update: admin panel

--- backend/controllers/foodController.js
+++ backend/controllers/foodController.js
@@ -26,4 +26,29 @@ const addFood = async (req, res) => {
 
 }
 
-export {addFood}
+// all food list
+const listFood = async (req, res) =>{
+    try{
+        const foods = await foodModel.find();
+        res.status(200).json({success:true, data: foods});
+    } catch(error){
+        console.log(error);
+        res.status(500).json({success:false, message: "Failed to list food items"});
+    }
+}
+
+// remove food item
+
+const removeFood = async (req, res) =>{
+    try{
+        const food = await foodModel.findByIdAndDelete(req.params.id);
+        fs.unlink(`uploads/${food.image}`,()=>{})
+
+        await foodModel.findByIdAndDelete(req.params.id);
+        res.status(200).json({success:true, message: "Food item removed successfully"});
+    } catch(error){
+        console.log(error);
+        res.status(500).json({success:false, message: "Failed to remove food item"});
+    }
+}
+export {addFood, listFood}
